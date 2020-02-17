fc2bf73d87cf69d681cc6bec54b279bbeb3b2ab6
Nico Vibert <nvibert@vmware.com>
2020-02-17 13:43:46 +0000

    Delete variables.tf

--- variables.tf
+++
@@ -1,4 +0,0 @@
-variable "data_center"        {default = "SDDC-Datacenter"}
-variable "cluster"            {default = "Cluster-1"}
-variable "workload_datastore" {default = "WorkloadDatastore"}
-variable "compute_pool"       {default = "Compute-ResourcePool"}
